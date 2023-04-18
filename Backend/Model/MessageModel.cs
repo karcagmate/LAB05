f19cff3aba0606211b625df88257c1a5cad5c297
Máté <karcagmate@stud.uni-obuda.hu>
2023-04-18 08:37:06 +0200

    SignalRHub created

--- Backend/Model/MessageModel.cs
+++ Backend/Model/MessageModel.cs
@@ -2,5 +2,12 @@
 {
     public class MessageModel
     {
+        public string Name { get; set; }
+        public string Message { get; set; }
+        public DateTime Date { get; set; }
+        public MessageModel()
+        {
+
+        }
     }
 }
